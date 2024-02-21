686e7acbfb7546736f4c4792f724b60279aeedb7
Bappy <najmul.hossain@enosisbd.com>
2024-02-21 15:26:58 +0600

    Refine some inconsistency

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,14 @@ document
   .appendChild(document.createTextNode(new Date().getFullYear()));
 
 // responsive
-function changeCaseStudy(x) {
-  if (x.matches) {
-    // If media query matches
-    document.getElementById("m-caseStudy").innerText = "c.study";
-  } else {
-    document.getElementById("m-caseStudy").innerText = "case study";
-  }
-}
+// function changeCaseStudy(x) {
+//   if (x.matches) {
+//     // If media query matches
+//     document.getElementById("m-caseStudy").innerText = "c.study";
+//   } else {
+//     document.getElementById("m-caseStudy").innerText = "case study";
+//   }
+// }
 var media600 = window.matchMedia("(max-width: 600px)");
 
-window.addEventListener("resize", changeCaseStudy(media600));
+// window.addEventListener("resize", changeCaseStudy(media600));
